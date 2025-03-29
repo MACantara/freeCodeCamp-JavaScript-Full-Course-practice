1a6d1da8748a44c79968721dff08ecab8c817ed1
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-03-29 18:26:55 +0800

    Add initial HTML, CSS, and JavaScript files for Blackjack game array practice

---
+++ Blackjack-Game/intro-to-arrays-practice/static/index.js
@@ -0,0 +1,9 @@
+let featuredPosts = ["post1", "post2", "post3", "post4", "post5", "post6"];
+
+let certifications = [
+    "CompTIA ITF+",
+    "CompTIA Cloud Essentials+",
+    "ITS HTML and CSS",
+    "ITS Databases",
+    "IC3 GS6 Level 1"
+]
