11103aab9f7d8ec1d9898d0421610c6a36b11820
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-03-29 17:24:40 +0800

    Add initial Blackjack game implementation with HTML, CSS, and JavaScript

---
+++ Blackjack-Game/static/index.js
@@ -0,0 +1,30 @@
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+let playRoundMessage = document.getElementById("play-round-message");
+let sumCounter = document.getElementById("sum-counter");
+let cardCollection = document.getElementById("card-collection");
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+
+    playRoundMessage.textContent = message;
+    cardCollection.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumCounter.textContent = "Sum: " + sum;
+
+    console.log("hasBlackJack: " + hasBlackJack); 
+    console.log("isAlive: " + isAlive);
+}
+
