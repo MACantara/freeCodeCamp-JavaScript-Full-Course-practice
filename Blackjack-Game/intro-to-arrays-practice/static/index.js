37558d13443794877977af23314fc317af3984e2
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-04-23 19:34:41 +0800

    Add array length check

--- Blackjack-Game/intro-to-arrays-practice/static/index.js
+++ Blackjack-Game/intro-to-arrays-practice/static/index.js
@@ -1,5 +1,3 @@
-let featuredPosts = ["post1", "post2", "post3", "post4", "post5", "post6"];
-
 let certifications = [
     "CompTIA ITF+",
     "CompTIA Cloud Essentials+",
@@ -8,7 +6,4 @@ let certifications = [
     "IC3 GS6 Level 1"
 ]
 
-console.log("Featured Posts: " + featuredPosts);
-console.log(certifications[1]);
-console.log(certifications[2]);
-console.log(certifications[0]);
+console.log(certifications.length);
