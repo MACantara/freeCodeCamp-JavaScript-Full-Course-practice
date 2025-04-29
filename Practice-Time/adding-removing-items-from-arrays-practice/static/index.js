c1a7c5768daa9da37f7e2283b115f56e3c92ad9d
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-04-29 13:54:21 +0800

    Add practice for adding and removing elements in an array

---
+++ Practice-Time/adding-removing-items-from-arrays-practice/static/index.js
@@ -0,0 +1,25 @@
+// let cards = [7, 4];
+
+// console.log(cards);
+
+// cards.push(6);
+
+// console.log(cards);
+
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+console.log(messages);
+
+let newMessage = "Same here!";
+
+messages.push(newMessage);
+
+console.log(messages);
+
+messages.pop();
+
+console.log(messages);
