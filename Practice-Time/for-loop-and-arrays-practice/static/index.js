f3e47e2a9f55c2178e6086bb1f8a1f58f406e543
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-05-01 22:51:40 +0800

    Add for loop and arrays practice

---
+++ Practice-Time/for-loop-and-arrays-practice/static/index.js
@@ -0,0 +1,18 @@
+// let messages = [
+//     "Hello",
+//     "I am a cat",
+//     "I am a dog",
+//     "I am a mouse"
+// ];
+
+// for (let i = 0; i < messages.length; i++)
+// {
+//     console.log(messages[i]);
+// }
+
+let cards = [7, 3, 9];
+
+for (let i = 0; i < cards.length; i++)
+{
+    console.log(cards[i]);
+}
