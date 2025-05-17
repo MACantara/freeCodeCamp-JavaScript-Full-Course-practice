2bac43095916a765c6581f1a9e7aa6b99f4eddd2
Michael Angelo R. Cantara <cantara.michaelangelo@gmail.com>
2025-05-17 17:21:43 +0800

    Move the start game variable initialization into the startGame function

--- Blackjack-Game/static/index.js
+++ Blackjack-Game/static/index.js
@@ -1,10 +1,7 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 
 let playRoundMessage = document.getElementById("play-round-message");
@@ -24,6 +21,12 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+    
     renderGame();
 }
 
