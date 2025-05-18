963189a09df5871babed01a64e9ef566c03725a5
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-05-18 13:22:31 +0800

    Add loops and arrays practice

---
+++ Practice-Time/loops-and-arrays-practice/static/index.js
@@ -0,0 +1,9 @@
+let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"];
+
+console.log("The 5 largest countries in the world:");
+console.log(largeCountries.length);
+
+for (let i = 0; i < largeCountries.length; i++)
+{
+    console.log(largeCountries[i]);
+}
