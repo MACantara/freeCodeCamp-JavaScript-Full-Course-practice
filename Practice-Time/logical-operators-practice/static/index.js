61e2e6bad98f9e0c321987fa00d4bcad320b4eb8
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-05-18 09:21:59 +0800

    Add and logical operation practice

---
+++ Practice-Time/logical-operators-practice/static/index.js
@@ -0,0 +1,10 @@
+let hasSolvedChallenge = false;
+let hasHintsLeft = false;
+
+function showSolution() {
+    console.log("Showing the solution...");
+}
+
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+    showSolution();
+}
