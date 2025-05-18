0a695425eb1f998b871c49a91f25cb366f8c0790
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-05-18 13:16:16 +0800

    Add objects and functions practice

---
+++ Practice-Time/objects-and-functions-practice/static/index.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Mik",
+    age: 22,
+    country: "Philippines"
+}
+
+function logData() {
+    console.log(person.name + " is " + person.age + " years old and lives in the " + person.country + ".");
+}
+
+logData();
