304dcf3c3a2658b6bb888b72730abe5a5f167f14
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-05-21 09:17:52 +0800

    Add <li> tags for the savedURLs

--- Chrome-Extension/static/index.js
+++ Chrome-Extension/static/index.js
@@ -13,5 +13,5 @@ function saveURL() {
 }
 
 for (let i = 0; i < savedURLs.length; i++) {
-    savedURLsList.textContent += savedURLs[i] + " ";
+    savedURLsList.innerHTML += "<li>" + savedURLs[i] + "</li>";
 }
