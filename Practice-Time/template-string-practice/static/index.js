8341d6db0eec458d8d69743ad4f157d1466e1ce5
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-05-21 13:19:55 +0800

    Add template string variable for sender

--- Practice-Time/template-string-practice/static/index.js
+++ Practice-Time/template-string-practice/static/index.js
@@ -1,5 +1,6 @@
 const recipient = "Jeong";
+const sender = "Mik";
 
-const email = `Hey ${recipient}! How is it going? Cheers Mik`;
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`;
 
 console.log(email);
